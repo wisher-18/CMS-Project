437c263f512e6d2ce326c33ea16a456cb6091c99
wisher-18 <johnnysonn10@gmail.com>
2023-11-07 23:59:44 +0530

    done

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -1,6 +1,46 @@
-<table class="table table-bordered table-hover">
-    <thead>
+<?php
+    if(isset($_POST['checkBoxArrayUsers'])){
+
+        foreach($_POST['checkBoxArrayUsers'] as $userIdValue){
+            $bulk_options_users = $_POST['bulk_options_users'];
+
+            switch($bulk_options_users){
+                case 'delete':
+                    $query = "DELETE FROM users WHERE user_id = {$userIdValue}";
+                    $bulk_delete_query = mysqli_query($connection, $query);
+                    if(!$bulk_delete_query){
+                        die("QUERY FAILED".mysqli_error($connection));
+                    }
+                    break;
+
+                default:
+            }
+        }
+    }
+?>
+
+
+
+
+<form action="" method="post">
+
+    <table class="table table-bordered table-hover">
+        <!-- Bulk Options For Edit -->
+        <div id="bulkOptionsContainerUsers" class="col-xs-4">
+            <select name="bulk_options_users" class="form-control">
+                <option value="">Select Options</option>
+                <option value="delete">Delete</option>
+            </select>
+        </div>
+
+        <div class="col-xs-4">
+            <input type="submit" name="submit" value="Apply" class="btn btn-success">
+            <a href="users.php?source=add_user" class="btn btn-primary">Add User</a>
+        </div>
+
+        <thead>
         <tr>
+            <th><input type="checkbox" id="selectAllBoxesUsers"></th>
             <th>User_Id</th>
             <th>Username</th>
             <th>First Name</th>
@@ -28,6 +68,13 @@
             $user_password = $row['user_password'];
             
             echo "<tr>";
+            ?>
+            <td><input type="checkbox" name="checkBoxArrayUsers[]" 
+            value="<?php echo $user_id; ?>" 
+            class="checkBoxes"></td>
+
+            <?php
+
             echo "<td>{$user_id}</td>";
             echo "<td>{$username}</td>";
             echo "<td>{$user_firstname}</td>";
@@ -36,15 +83,15 @@
             // $query = "SELECT * FROM posts WHERE post_id = $comment_post_id";
             // $select_post_id_query = mysqli_query($connection, $query);
             // while($row = mysqli_fetch_assoc($select_post_id_query)){
-            //     $post_id = $row['post_id'];
-            //     $post_title = $row['post_title'];
-
-            //     echo "<td><a href='../post.php?p_id=$post_id'>$post_title</a></td>";
-
-            // }
-            echo "<td>{$user_role}</td>";
-            echo "<td><a href='users.php?change_to_admin={$user_id}'>Admin</a></td>";
-            echo "<td><a href='users.php?change_to_sub={$user_id}'>Subs</a></td>";
+                //     $post_id = $row['post_id'];
+                //     $post_title = $row['post_title'];
+                
+                //     echo "<td><a href='../post.php?p_id=$post_id'>$post_title</a></td>";
+                
+                // }
+                echo "<td>{$user_role}</td>";
+                echo "<td><a href='users.php?change_to_admin={$user_id}'>Admin</a></td>";
+                echo "<td><a href='users.php?change_to_sub={$user_id}'>Subs</a></td>";
             echo "<td><a href='users.php?source=edit_user&edit_user={$user_id}'>Edit</a></td>";
             echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete this user?');\" href='users.php?delete={$user_id}'>Delete</a></td>";
             echo "</tr>";
@@ -53,8 +100,9 @@
 
         ?>
 
-    </tbody>
+</tbody>
 </table>
+</form>
 
 <?php
 //APPROVING COMMENTS QUERY
